6da653d978f5a367ef7df4f8d83e96713c43d296
Simon Nguyen <simon@simtechmedia.com>
2013-11-26 21:18:20 +1100

    updated angular 1.2.2 to latest version

---
+++ app/scripts/animations/animations.js
@@ -0,0 +1,10 @@
+/*'use strict';
+angular.module('simangApp')
+    .animation('fade-in', function($window) {
+        return {
+            start : function(element, done) {
+                TweenMax.set(element, {opacity:'0'});
+                TweenMax.to(element, 1, {opacity:1});
+            }
+        }
+    })*/
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('simangApp', [])
+angular.module('simangApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
